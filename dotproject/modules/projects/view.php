f48543025d0f68ad203c08fc13dacf05e62f82ac
cyberhorse <cyberhorse@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2005-05-09 10:51:26 +0000

    as suggested in mantis #809
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@3306 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/projects/view.php
+++ dotproject/modules/projects/view.php
@@ -158,7 +158,7 @@ if ($canEdit) {
 	if ($canDelete) {
 		$titleBlock->addCrumbDelete( 'delete project', $canDelete, $msg );
 	}
-	$titleBlock->addCrumb("?m=tasks&a=organize&project_id=$project_id", "move/copy tasks");
+	$titleBlock->addCrumb("?m=tasks&a=organize&project_id=$project_id", "organize tasks");
 }
 $titleBlock->addCrumb( "?m=projects&a=reports&project_id=$project_id", "reports" );
 $titleBlock->show();
