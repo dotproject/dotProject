6cfd2ee3ecc4fae97d0fd826fe7ab2def8121029
gregorerhardt <gregorerhardt@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-02-27 09:34:10 +0000

    reset company and dep id which is unused and has been biased by info from another tab
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@3898 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/projects/admin_tab.viewuser.projects_gantt.php
+++ dotproject/modules/projects/admin_tab.viewuser.projects_gantt.php
@@ -1,5 +1,9 @@
 <?php /* TASKS $Id$gantt.php,v 1.30 2004/08/06 22:56:54 gregorerhardt Exp $ */
 GLOBAL  $company_id, $dept_ids, $department, $min_view, $m, $a, $user_id, $tab;
 
+// reset the department and company filter info
+// which is not used here
+$company_id = $department = 0;
+
 require("{$dPconfig['root_dir']}/modules/projects/viewgantt.php");
 ?>
