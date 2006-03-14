bfcdbfecf0a0ae2f907c0409ac98cec2b5f561e5
gregorerhardt <gregorerhardt@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-03-14 13:07:40 +0000

    corrected tab link for permissions tab
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4033 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/admin/vw_usr.php
+++ dotproject/modules/admin/vw_usr.php
@@ -37,7 +37,7 @@ foreach ($users as $row) {
 				</a>
 			</td>
 			<td>
-				<a href="?m=admin&a=viewuser&user_id=<?php echo $row["user_id"];?>&tab=2" title="">
+				<a href="?m=admin&a=viewuser&user_id=<?php echo $row["user_id"];?>&tab=3" title="">
 					<img src="images/obj/lock.gif" width="16" height="16" border="0" alt="<?php echo $AppUI->_('edit permissions');?>">
 				</a>
 			</td>
