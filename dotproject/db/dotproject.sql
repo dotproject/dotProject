3c422799ad46e09cecdee41780e27a640082ed8d
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-07-06 20:29:13 +0000

    Fixed trivial bug #1714:
    changed version number string from '2.0.2' to '2.0.4'
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4338 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/db/dotproject.sql
+++ dotproject/db/dotproject.sql
@@ -1191,7 +1191,7 @@ CREATE TABLE dpversion (
 	last_code_update date not null default '0000-00-00'
 );
 
-INSERT INTO dpversion VALUES ('2.0.2', 2, '2006-04-21', '2006-04-21');
+INSERT INTO dpversion VALUES ('2.0.4', 2, '2006-04-21', '2006-04-21');
 
 # 20050307
 # Additional LDAP search user and search password fields for Active Directory compatible LDAP authentication
