4261c3488a82130b7cae2e23f379145d18323863
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-07-12 04:30:42 +0000

    Reordered query to get around LEFT JOIN constraints in MySQL 5
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4347 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/tasks.class.php
+++ dotproject/modules/tasks/tasks.class.php
@@ -1350,9 +1350,10 @@ class CTask extends CDpObject {
 
         function getAssignedUsers(){
                 $sql = "select u.*, ut.perc_assignment, ut.user_task_priority, co.contact_last_name
-                        from users as u, user_tasks as ut
-            LEFT JOIN contacts as co ON co.contact_id = u.user_contact
-                        where ut.task_id = '$this->task_id'
+                        from users as u
+			LEFT JOIN contacts as co ON co.contact_id = u.user_contact,
+                        user_tasks as ut
+			where ut.task_id = '$this->task_id'
                               and ut.user_id = u.user_id";
                 return db_loadHashList($sql, "user_id");
         }
