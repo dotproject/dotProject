fa42c2989084ec4d6e9834d4cbba5e8b862106a7
nybod <nybod@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-07-31 11:54:15 +0000

    Function 'findchilddept' renamed to avoid naming clash when viewed in flat view (fix taken from HEAD code)
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4383 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/companies/vw_depts.php
+++ dotproject/modules/companies/vw_depts.php
@@ -14,22 +14,22 @@ $q->addOrder('dept_parent, dept_name');
 $sql = $q->prepare();
 $q->clear();
 
-function showchilddept( &$a, $level=0 ) {
+// function renamed to avoid naming clash
+function showchilddept_comp( &$a, $level=0 ) {
 	global $AppUI;
-	$s = '';
+	$s = '
+	<td>
+		<a href="./index.php?m=departments&amp;a=addedit&amp;dept_id='.$a["dept_id"].'" title="'.$AppUI->_('edit').'">
+			' . dPshowImage( './images/icons/stock_edit-16.png', 16, 16, '' ) . '
+	</td>
+	<td>';
 
-	$s .= '<td>';
-	$s .= '<a href="./index.php?m=departments&a=addedit&dept_id='.$a["dept_id"].'" title="'.$AppUI->_('edit').'">';
-	$s .= dPshowImage( './images/icons/stock_edit-16.png', 16, 16, '' );
-	$s .= '</td>';
-	$s .= '<td>';
-
-	for ($y=0; $y < $level; $y++) {
-		if ($y+1 == $level) {
+	for ($y=0; $y < $level; $y++) 
+	{
+		if ($y+1 == $level)
 			$s .= '<img src="./images/corner-dots.gif" width="16" height="12" border="0">';
-		} else {
+		else
 			$s .= '<img src="./images/shim.gif" width="16" height="12" border="0">';
-		}
 	}
 
 	$s .= '<a href="./index.php?m=departments&a=view&dept_id='.$a["dept_id"].'">'.$a["dept_name"].'</a>';
@@ -39,13 +39,14 @@ function showchilddept( &$a, $level=0 ) {
 	echo "<tr>$s</tr>";
 }
 
-function findchilddept( &$tarr, $parent, $level=0 ){
+// function renamed to avoid naming clash
+function findchilddept_comp( &$tarr, $parent, $level=0 ){
 	$level = $level+1;
 	$n = count( $tarr );
 	for ($x=0; $x < $n; $x++) {
 		if($tarr[$x]["dept_parent"] == $parent && $tarr[$x]["dept_parent"] != $tarr[$x]["dept_id"]){
-			showchilddept( $tarr[$x], $level );
-			findchilddept( $tarr, $tarr[$x]["dept_id"], $level);
+			showchilddept_comp( $tarr[$x], $level );
+			findchilddept_comp( $tarr, $tarr[$x]["dept_id"], $level);
 		}
 	}
 }
@@ -59,7 +60,7 @@ if (count( $rows)) {
 	$s .= '<th width="100%">'.$AppUI->_( 'Name' ).'</th>';
 	$s .= '<th>'.$AppUI->_( 'Users' ).'</th>';
 } else {
-	$s .= $AppUI->_('No data available');
+	$s .= '<td>' . $AppUI->_('No data available') . '</td>';
 }
 
 $s .= '</tr>';
@@ -67,16 +68,19 @@ echo $s;
 
 foreach ($rows as $row) {
 	if ($row["dept_parent"] == 0) {
-		showchilddept( $row );
-		findchilddept( $rows, $row["dept_id"] );
+		showchilddept_comp( $row );
+		findchilddept_comp( $rows, $row["dept_id"] );
 	}
 }
 
-echo '<td colspan="3" nowrap="nowrap" rowspan="99" align="right" valign="top" style="background-color:#ffffff">';
+echo '
+<tr>
+	<td colspan="3" nowrap="nowrap" rowspan="99" align="right" valign="top" style="background-color:#ffffff">';
 if ($canEdit) {
-	echo '<input type="button" class=button value="'.$AppUI->_( 'new department' ).'" onClick="javascript:window.location=\'./index.php?m=departments&a=addedit&company_id='.$company_id.'\';">';
+	echo '<input type="button" class=button value="'.$AppUI->_( 'new department' ).'" onclick="javascript:window.location=\'./index.php?m=departments&amp;a=addedit&amp;company_id='.$company_id.'\';" />';
 }
-echo '</td>';
-
-echo '</table>';
+echo '
+	</td>
+</tr>
+</table>';
 ?>
