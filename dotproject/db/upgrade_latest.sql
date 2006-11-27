46a44fec9d4e856918a0709ab867cdf8128da87d
gregorerhardt <gregorerhardt@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2006-11-27 15:28:51 +0000

    remove installer inconsistencies
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4572 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/db/upgrade_latest.sql
+++ dotproject/db/upgrade_latest.sql
@@ -6,13 +6,11 @@
 # All entries must be date stamped in the correct format.
 #
 
-# 20061119 gregorerhardt
+# 20060809
+ALTER TABLE `sessions` ADD `session_user` INT DEFAULT '0' NOT NULL AFTER `session_id`;
+
+# 20061119 
 # archived status replaces project (in)active flag:
-# add project archived status
-# migrate existing project data
-# remove project in/active flag
 UPDATE `sysvals` SET `sysval_value` = '0|Not Defined\r\n1|Proposed\r\n2|In Planning\r\n3|In Progress\r\n4|On Hold\r\n5|Complete\r\n6|Template\r\n7|Archived' WHERE `sysval_id` = 1 LIMIT 1;
-
 UPDATE `projects` SET `project_status` = 7 WHERE `project_active` = 0;
-
 ALTER TABLE `projects` DROP `project_active`;
