577fd84cb9e205b19e70474fd674b81865499067
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-01-02 17:14:03 +0000

    Updated "$utypes" user type array to use system configured values instead of using a predefined list as it had been.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4641 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/functions/admin_func.php
+++ dotproject/functions/admin_func.php
@@ -1,18 +1,6 @@
 <?php
 // user types
-$utypes = array(
-// DEFAULT USER (nothing special)
-	0 => '',
-// DO NOT CHANGE ADMINISTRATOR INDEX !
-	1 => 'Administrator',
-// you can modify the terms below to suit your organisation
-	2 => 'CEO',
-	3 => 'Director',
-	4 => 'Branch Manager',
-	5 => 'Manager',
-	6 => 'Supervisor',
-	7 => 'Employee'
-);
+$utypes = dPgetSysVal( 'UserType' );
 
 ##
 ##	NOTE: the user_type field in the users table must be changed to a TINYINT
