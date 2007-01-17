bad1d877f4d1768875029bf9f617806d41d89c7d
nybod <nybod@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-01-17 12:56:02 +0000

    Mantis 1833 regarding task log editing permissions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4655 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/projects/vw_logs.php
+++ dotproject/modules/projects/vw_logs.php
@@ -129,7 +129,7 @@ foreach ($logs as $row) {
 
 	$s .= '<tr bgcolor="white" valign="top">';
 	$s .= "\n\t<td>";
-	if ($perms->checkModuleItem($m, 'edit', $row['task_id']) ) {
+	if ($perms->checkModuleItem('tasks', 'edit', $row['task_id']) ) {
 		$s .= "\n\t\t<a href=\"?m=tasks&a=view&task_id=".$row['task_id']."&tab=1&task_log_id=".@$row['task_log_id']."\">"
 			. "\n\t\t\t". dPshowImage( './images/icons/stock_edit-16.png', 16, 16, '' )
 			. "\n\t\t</a>";
