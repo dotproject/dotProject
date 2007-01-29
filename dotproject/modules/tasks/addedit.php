241017b21e094a91836a19de446e3636eaf01ba3
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-01-29 23:50:07 +0000

    Fixed Mantis Bug 1857: Task name unnecessarily being run through dPformSafe() on Add/Edit display page.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4700 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/addedit.php
+++ dotproject/modules/tasks/addedit.php
@@ -262,7 +262,7 @@ var daily_working_hours = <?php echo intval(dPgetConfig('daily_working_hours'));
 <tr valign="top" width="50%">
 	<td>
 		<?php echo $AppUI->_( 'Task Name' );?> *
-		<br /><input type="text" class="text" name="task_name" value="<?php echo dPformSafe( $obj->task_name );?>" size="40" maxlength="255" />
+		<br /><input type="text" class="text" name="task_name" value="<?php echo( $obj->task_name );?>" size="40" maxlength="255" />
 	</td>
 	<td>
 		<table cellspacing="0" cellpadding="2" border="0" width="100%">
