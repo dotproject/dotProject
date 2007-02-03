f798dde41e19950faaf905d19c581b24b791f7a5
gregorerhardt <gregorerhardt@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-02-03 10:48:47 +0000

    corrected typo in basedir define
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4721 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/install/check_upgrade.php
+++ dotproject/install/check_upgrade.php
@@ -39,7 +39,7 @@ The full text of the GPL is in the COPYING file.
 
 require_once 'install.inc.php';
 $baseDir = dirname(dirname(__FILE__));
-defined('DP_BASE_DIR', $baseDir);
+define('DP_BASE_DIR', $baseDir);
 require_once "$baseDir/lib/adodb/adodb.inc.php";
 
 function dPcheckExistingDB($conf) {
