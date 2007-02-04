09f59cfd63bd29e60fa3e30d7c156b5585cc0e5a
gregorerhardt <gregorerhardt@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-02-04 11:37:22 +0000

    1799 - sql error on saving with inexisting task dependency
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4734 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/do_task_aed.php
+++ dotproject/modules/tasks/do_task_aed.php
@@ -152,7 +152,7 @@ if ($sub_form) {
 			$obj->updateAssigned( $hassign , $hperc_assign_ar);
 		}
 		
-		if (isset($hdependencies)) {
+		if (isset($hdependencies) && !empty($hdependencies)) {
 			// there are dependencies set!
 			
 			// backup initial start and end dates
