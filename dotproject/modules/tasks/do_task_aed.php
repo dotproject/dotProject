760c4df8d54152f11f4f43d706da3e2438cb5e84
caseydk <caseydk@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-02-05 18:18:17 +0000

    fix from #1695 - when moving a task to a new project, it resets its task_parent - was causing odd display errors elsewhere
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4737 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/do_task_aed.php
+++ dotproject/modules/tasks/do_task_aed.php
@@ -67,8 +67,10 @@ if ($sub_form) {
 	}
 		
 	// Check to see if the task_project has changed
-	if (isset($_POST['new_task_project']) && $_POST['new_task_project'])
+	if (isset($_POST['new_task_project']) && $_POST['new_task_project']) {
 		$obj->task_project = $_POST['new_task_project'];
+    $obj->task_parent  = $obj->task_id;
+  }
 
 	// Map task_dynamic checkboxes to task_dynamic values for task dependencies.
 	if ( $obj->task_dynamic != 1 ) {
