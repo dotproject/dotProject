96c2ca02a073a549bcb4c74ad0f3906013296c43
caseydk <caseydk@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-05-09 15:11:01 +0000

    tweak for the includes
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4902 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/ticketsmith/comment.php
+++ dotproject/modules/ticketsmith/comment.php
@@ -16,8 +16,8 @@ $titleBlock->addCrumb( "?m=ticketsmith", "tickets list" );
 $titleBlock->addCrumb( "?m=ticketsmith&a=view&ticket=$ticket", "view this ticket" );
 $titleBlock->show();
 
-require(DP_BASE_DIR.'modules/ticketsmith/config.inc.php');
-require(DP_BASE_DIR.'modules/ticketsmith/common.inc.php');
+require(DP_BASE_DIR.'/modules/ticketsmith/config.inc.php');
+require(DP_BASE_DIR.'/modules/ticketsmith/common.inc.php');
 
 /* set title */
 $title = "Post Comment";
