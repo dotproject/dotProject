9671561f2d61e51e873f3edd62a7144e6ae77f8e
cyberhorse <cyberhorse@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-05-28 11:13:02 +0000

    #2012 - fixed mishandling single quotes on the system configuration page
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@4961 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/system/systemconfig.php
+++ dotproject/modules/system/systemconfig.php
@@ -6,7 +6,7 @@ if (!defined('DP_BASE_DIR')) {
 
 // check permissions
 if (!$canEdit) {
-    $AppUI->redirect( "m=public&a=access_denied" );
+    $AppUI->redirect( 'm=public&a=access_denied' );
 }
 
 $dPcfg = new CConfig();
@@ -22,17 +22,17 @@ $tab = $AppUI->getState( 'ConfigIdxTab' ) !== NULL ? $AppUI->getState( 'ConfigId
 $active = intval( !$AppUI->getState( 'ConfigIdxTab' ) );
 
 $titleBlock = new CTitleBlock('System Configuration', 'control-center.png', $m);
-$titleBlock->addCrumb( "?m=system", "system admin" );
-$titleBlock->addCrumb( "?m=system&a=addeditpref", "default user preferences" );
+$titleBlock->addCrumb( '?m=system', 'system admin' );
+$titleBlock->addCrumb( '?m=system&a=addeditpref', 'default user preferences' );
 $titleBlock->show();
 
-if (is_dir(DP_BASE_DIR."/install")) {
-	$AppUI->setMsg("You have not removed your install directory, this is a major security risk!", UI_MSG_ALERT);
-	echo "<span class='error'>" . $AppUI->getMsg() . "</span>\n";
+if (is_dir(DP_BASE_DIR.'/install')) {
+	$AppUI->setMsg('You have not removed your install directory, this is a major security risk!', UI_MSG_ALERT);
+	echo '<span class="error">' . $AppUI->getMsg() . '</span>'."\n";
 }
 
-echo $AppUI->_("syscfg_intro");
-echo "<br />&nbsp;<br />";
+echo $AppUI->_('syscfg_intro');
+echo '<br />&nbsp;<br />';
 
 
 // prepare the automated form fields based on db system configuration data
@@ -40,47 +40,47 @@ $output  = null;
 $last_group = '';
 foreach ($rs as $c) {
 
- 	$tooltip = "title='".$AppUI->_($c['config_name'].'_tooltip')."'";
+ 	$tooltip = 'title="'.$AppUI->_($c['config_name'].'_tooltip').'"';
 	// extraparse the checkboxes and the select lists
 	$value = '';
 	switch ($c['config_type']) {
 		case 'select':
 			// Build the select list.
-			$entry = "<select class='text' name='dPcfg[{$c['config_name']}]'>\n";
+			$entry = '<select class="text" name="dPcfg['.$c['config_name'].']">'."\n";
 			// Find the detail relating to this entry.
 			$children = $dPcfg->getChildren($c['config_id']);
 			foreach ($children as $child) {
-				$entry .= "<option value='{$child['config_list_name']}'";
+				$entry .= '<option value="'.$child['config_list_name'].'"';
 				if ($child['config_list_name'] == $c['config_value'])
-					$entry  .= " selected='selected'";
-				$entry .= ">" . $AppUI->_($child['config_list_name'] . '_item_title') . "</option>\n";
+					$entry  .= ' selected="selected"';
+				$entry .= '>' . $AppUI->_($child['config_list_name'] . '_item_title') . '</option>'."\n";
 			}
-			$entry .= "</select>";
+			$entry .= '</select>';
 			break;
 		case 'checkbox':
-			$extra = ($c['config_value'] == 'true') ? "checked='checked'" : '';
+			$extra = ($c['config_value'] == 'true') ? 'checked="checked"' : '';
 			$value = 'true';
 			// allow to fallthrough
 		default:
 			if (! $value)
 				$value = $c['config_value'];
-			$entry = "<input class='button' type='{$c['config_type']}' name='dPcfg[{$c['config_name']}]' value='$value' $tooltip $extra/>";
+			$entry = '<input class="button" type="'.$c['config_type'].'" name="dPcfg['.$c['config_name'].']" value="'.$value.'" '.$tooltip.' '. $extra.'/>';
 			break;
 	}
 
 	if ($c['config_group'] != $last_group) {
-		$output .="<tr><td colspan='2'><b>" . $AppUI->_($c['config_group'] .'_group_title') . "</b></td></tr>\n";
+		$output .= '<tr><td colspan="2"><b>' . $AppUI->_($c['config_group'] .'_group_title') . '</b></td></tr>'."\n";
 		$last_group = $c['config_group'];
 	}
-	$output .= "<tr>
-			<td class='item' width='20%'>".$AppUI->_($c['config_name'].'_title')."</td>
-            		<td align='left'>
-				$entry
-				<a href='#' onClick=\"javascript:window.open('?m=system&a=systemconfig_help&dialog=1&cn={$c['config_name']}', 'contexthelp', 'width=400, height=200, left=50, top=50, scrollbars=yes, resizable=yes')\" $tooltip>(?)</a>
-				<input class='button' type='hidden'  name='dPcfgId[{$c['config_name']}]' value='{$c['config_id']}' />
+	$output .= '<tr>
+			<td class="item" width="20%">'.$AppUI->_($c['config_name'].'_title').'</td>
+            		<td align="left">
+' .				$entry . '
+				<a href="#" onClick="javascript:window.open(\'?m=system&a=systemconfig_help&dialog=1&cn='.$c['config_name'].'\', \'contexthelp\', \'width=400, height=200, left=50, top=50, scrollbars=yes, resizable=yes\')" '.$tooltip.'>(?)</a>
+				<input class="button" type="hidden"  name="dPcfgId['.$c['config_name'].']" value="'.$c['config_id'].'" />
 			</td>
         </tr>
-	";
+	';
 
 	}
 echo '<form name="cfgFrm" action="index.php?m=system&a=systemconfig" method="post">';
