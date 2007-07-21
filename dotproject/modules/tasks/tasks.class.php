2d40de2891a2e2ae3c8d7e9fb8b09a45f5b68edf
nybod <nybod@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-07-21 13:13:42 +0000

    Mantis 2098. Remove create log link if task is dynamic (task_dynamic == 1)
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5188 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/tasks.class.php
+++ dotproject/modules/tasks/tasks.class.php
@@ -1929,11 +1929,11 @@ function showtask( &$a, $level=0, $is_opened = true, $today_view = false) {
         $s .= dPshowImage( './images/icons/dialog-warning5.png', 16, 16, 'Problem', 'Problem!' );
         $s .='</a></td>';
     } 
-    else if ($canViewLog) {
-        $s .= "\n\t<td><a href=\"?m=tasks&a=view&task_id=" . $a['task_id'] . '&tab=1">'.$AppUI->_('Log').'</a></td>';
+    else if ($canViewLog && $a["task_dynamic"]!=1) {
+        $s .= "\n\t<td align='center'><a href=\"?m=tasks&a=view&task_id=" . $a['task_id'] . '&tab=1">'.$AppUI->_('Log').'</a></td>';
     } 
     else {
-        $s .= "\n\t<td></td>";
+        $s .= "\n\t<td align='center'>" . $AppUI->_('-') . "</td>";
     }
     // percent complete
     $s .= "\n\t<td align=\"right\">".intval( $a["task_percent_complete"] ).'%</td>';
