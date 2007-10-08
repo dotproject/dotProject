9d907e01326f494dfe812dedc18f00adeb7898b4
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-10-08 04:17:38 +0000

    #2170: Fix problem with lostpass page not showing when default style is dp-grey-theme
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5408 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/style/dp-grey-theme/lostpass.php
+++ dotproject/style/dp-grey-theme/lostpass.php
@@ -4,9 +4,9 @@
 	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title><?php echo @$AppUI->getConfig( 'page_title' );?></title>
+	<title><?php echo @dPgetConfig( 'page_title' );?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
-       	<title><?php echo $AppUI->cfg['company_name'];?> :: dotProject Login</title>
+       	<title><?php echo $dPconfig['company_name'];?> :: dotProject Login</title>
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta name="Version" content="<?php echo @$AppUI->getVersion();?>" />
 	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
@@ -23,7 +23,7 @@
 <input type="hidden" name="lostpass" value="1" />
 <input type="hidden" name="redirect" value="<?php echo $redirect;?>" />
 <tr>
-	<th colspan="2"><em><?php echo $AppUI->cfg['company_name'];?></em></th>
+	<th colspan="2"><em><?php echo $dPconfig['company_name'];?></em></th>
 </tr>
 <tr>
 	<td align="right" nowrap><?php echo $AppUI->_('Username');?>:</td>
