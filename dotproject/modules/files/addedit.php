9ec55e60b355b876c5d9723b6459049ebd3e4af4
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-10-10 17:38:46 +0000

    The function projectSelectWithOptGroup() is defined in includes/main_functions.php and was throwing since it had been defined in here as well.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5414 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/files/addedit.php
+++ dotproject/modules/files/addedit.php
@@ -374,28 +374,4 @@ function getHelpdeskFolder() {
 	return intval($ffid);
 }
 
-function projectSelectWithOptGroup( $user_id, $select_name, $select_attribs, $selected ) {
-	global $AppUI ;
-	$q = new DBQuery();
-	$q->addTable('projects');
-	$q->addQuery('project_id, co.company_name, project_name');
-	$proj = new CProject();
-	$proj->setAllowedSQL( $user_id, $q );
-	$q->addOrder('co.company_name');
-	$projects = $q->loadList();
-	$s = "\n<select name=\"$select_name\" $select_attribs>";
-	$s .= "\n\t<option value=\"0\" "  . ( $selected == 0 ? "selected=\"selected\"" : "" ) ." >" . $AppUI->_('None') . "</option>" ;
-	$current_company = "";
-	foreach ($projects as $p )
-		{
-		if ( $p['company_name'] != $current_company )
-			{
-			$current_company = $p['company_name'];
-			$s .= "\n<optgroup label=\""  . $current_company . "\" >" . $current_company. "</optgroup>" ;
-			}
-		$s .= "\n\t<option value=\"".$p['project_id']."\"".($selected == $p['project_id'] ? " selected=\"selected\"" : '').">&nbsp;&nbsp;&nbsp;" .  $p['project_name'] . "</option>";
-		}
-	$s .= "\n</select>\n";
-	return $s;
-}
 ?>
