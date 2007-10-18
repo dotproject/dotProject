2942337a9adcf5b1cef32182e461ede342f440ec
nybod <nybod@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2007-10-18 14:27:11 +0000

    Mantis 2181
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5443 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/companies/vw_active.php
+++ dotproject/modules/companies/vw_active.php
@@ -23,6 +23,14 @@ $q->addQuery('project_id, project_name, project_start_date, project_status, proj
 $q->addJoin('users', 'u', 'u.user_id = projects.project_owner');
 $q->addJoin('contacts', 'con', 'u.user_contact = con.contact_id');
 $q->addWhere('projects.project_company = '.$company_id);
+
+include_once ( $AppUI->getModuleClass('projects'));
+$projObj = new CProject();
+$projList = $projObj->getDeniedRecords($AppUI->user_id );
+if ( count($projList) ) {
+$q->addWhere('NOT (project_id IN (' . implode(',',$projList) .  ') )') ;
+}
+
 $q->addWhere('projects.project_status <> 7');
 $q->addOrder($sort);
 $s = '';
