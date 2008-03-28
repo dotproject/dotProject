1d9303714109ce1cb0a88b36633c0e0efcf80c0d
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2008-03-28 17:06:54 +0000

    Replaced check against function call to checkModuleItem() with check against variable containing the return value of the same call to checkModuleItem().
    This call is now done once and it's return value stored 
    before the foreach loop it is used within rather than 
    it being called for each interation.
    
    Also some minor formatting clean-up.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5649 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/vw_logs.php
+++ dotproject/modules/tasks/vw_logs.php
@@ -3,7 +3,7 @@ if (!defined('DP_BASE_DIR')){
   die('You should not access this file directly.');
 }
 
-global $AppUI, $task_id, $df, $canEdit, $m;
+global $AppUI, $task_id, $df, $m;
 
 $perms =& $AppUI->acl();
 if (! $perms->checkModuleItem('task_log', 'view', $task_id)) {
@@ -14,6 +14,7 @@ $problem = intval( dPgetParam( $_GET, 'problem', null ) );
 // get sysvals
 $taskLogReference = dPgetSysVal( 'TaskLogReference' );
 $taskLogReferenceImage = dPgetSysVal( 'TaskLogReferenceImage' );
+
 ?>
 <script language="JavaScript">
 <?php
@@ -21,6 +22,8 @@ $taskLogReferenceImage = dPgetSysVal( 'TaskLogReferenceImage' );
 // some javascript functions may not appear on client side in case of user not having write permissions
 // else users would be able to arbitrarily run 'bad' functions
 $canDelete = $perms->checkModuleItem('task_log', 'delete', $task_id);
+$canEdit = $perms->checkModuleItem('task_log', 'edit', $task_id);
+
 if ($canDelete) {
 ?>
 function delIt2(id) {
@@ -42,9 +45,9 @@ function delIt2(id) {
 <tr>
 	<th></th>
 	<th><?php echo $AppUI->_('Date');?></th>
-        <th title="<?php echo $AppUI->_('Reference');?>"><?php echo $AppUI->_('Ref');?></th>
+	<th title="<?php echo $AppUI->_('Reference');?>"><?php echo $AppUI->_('Ref');?></th>
 	<th width="100"><?php echo $AppUI->_('Summary');?></th>
-    <th><?php echo $AppUI->_('URL');?></th>
+	<th><?php echo $AppUI->_('URL');?></th>
 	<th width="100"><?php echo $AppUI->_('User');?></th>
 	<th width="100"><?php echo $AppUI->_('Hours');?></th>
 	<th width="100"><?php echo $AppUI->_('Cost Code');?></th>
@@ -67,40 +70,37 @@ $s = '';
 $hrs = 0;
 foreach ($logs as $row) {
 	$task_log_date = intval( $row['task_log_date'] ) ? new CDate( $row['task_log_date'] ) : null;
-        $style = $row['task_log_problem'] ? 'background-color:#cc6666;color:#ffffff' :'';
+		$style = $row['task_log_problem'] ? 'background-color:#cc6666;color:#ffffff' :'';
 
 	$s .= '<tr bgcolor="white" valign="top">';
 	$s .= "\n\t<td>";
-	if ($perms->checkModuleItem('task_log', 'edit', $task_id)) {
-		if ($tab == -1) {
-		 	$s .= "\n\t\t<a href=\"?m=tasks&a=view&task_id=$task_id&tab=".$AppUI->getState( 'TaskLogVwTab' );
-		} else {
-			$s .= "\n\t\t<a href=\"?m=tasks&a=view&task_id=$task_id&tab=1";
-
-		}
-		 $s .= "&task_log_id=".@$row['task_log_id']."#log\">"
-			. "\n\t\t\t". dPshowImage( './images/icons/stock_edit-16.png', 16, 16, '' )
-			. "\n\t\t</a>";
+	if ($canEdit) {
+		$s .= ("\n\t\t<a href=\"?m=tasks&a=view&task_id=$task_id&tab=" 
+			   . (($tab == -1) ? $AppUI->getState( 'TaskLogVwTab' ) : '1') 
+			   . "&task_log_id=".@$row['task_log_id']."#log\">" . "\n\t\t\t" 
+			   . dPshowImage('./images/icons/stock_edit-16.png', 16, 16, ''). "\n\t\t</a>");
 	}
-        $s .= "\n\t</td>";
-	$s .= '<td nowrap="nowrap">'.($task_log_date ? $task_log_date->format( $df ) : '-').'</td>';
-        //$s .= '<td align="center" valign="middle">'.($row['task_log_problem'] ?  dPshowImage( './images/icons/mark-as-important-16.png', 16, 16, 'Problem', 'Problem' ) : '').'</td>';
-        $reference_image = "-";
-        if($row["task_log_reference"] > 0){
-            if(isset($taskLogReferenceImage[$row["task_log_reference"]])){
-                $reference_image = dPshowImage( $taskLogReferenceImage[$row["task_log_reference"]], 16, 16, $taskLogReference[$row["task_log_reference"]], $taskLogReference[$row["task_log_reference"]] );
-            } else if (isset($taskLogReference[$row["task_log_reference"]])){
-                $reference_image = $taskLogReference[$row["task_log_reference"]];
-            }
-        }
-        $s .= '<td align="center" valign="middle">'.$reference_image.'</td>';
+	$s .= "\n\t</td>";
+	$s .= '<td nowrap="nowrap">'.(($task_log_date) ? $task_log_date->format( $df ) : '-').'</td>';
+	//$s .= '<td align="center" valign="middle">'.($row['task_log_problem'] ?  dPshowImage( './images/icons/mark-as-important-16.png', 16, 16, 'Problem', 'Problem' ) : '').'</td>';
+	$reference_image = "-";
+	if($row["task_log_reference"] > 0){
+		if (isset($taskLogReferenceImage[$row["task_log_reference"]])) {
+			$reference_image = dPshowImage($taskLogReferenceImage[$row["task_log_reference"]], 16, 16, 
+										   $taskLogReference[$row["task_log_reference"]], 
+										   $taskLogReference[$row["task_log_reference"]]);
+		} else if (isset($taskLogReference[$row["task_log_reference"]])){
+			$reference_image = $taskLogReference[$row["task_log_reference"]];
+		}
+	}
+	$s .= '<td align="center" valign="middle">'.$reference_image.'</td>';
 	$s .= '<td width="30%" style="'.$style.'">'.@$row["task_log_name"].'</td>';
-    $s .= !empty($row["task_log_related_url"]) ? '<td><a href="'.@$row["task_log_related_url"].'" title="'.@$row["task_log_related_url"].'">'.$AppUI->_('URL').'</a></td>' : '<td></td>';
+	$s .= !empty($row["task_log_related_url"]) ? '<td><a href="'.@$row["task_log_related_url"].'" title="'.@$row["task_log_related_url"].'">'.$AppUI->_('URL').'</a></td>' : '<td></td>';
 	$s .= '<td width="100">'.$row["user_username"].'</td>';
 	$s .= '<td width="100" align="right">'.sprintf( "%.2f", $row["task_log_hours"] ) . '<br />(';
-	$minutes = (int) (( $row["task_log_hours"] - ((int)  $row["task_log_hours"] ))*60);
-	$minutes = ((strlen($minutes) == 1) ? ('0'.$minutes) : $minutes);
-	$s .= (int)  $row["task_log_hours"] .':'. $minutes . ')</td>';
+	$minutes = (int) (($row["task_log_hours"] - ((int) $row["task_log_hours"])) * 60);
+	$minutes = ((strlen($minutes) == 1) ? ('0' . $minutes) : $minutes);
+	$s .= (int) $row["task_log_hours"] .':'. $minutes . ')</td>';
 	$s .= '<td width="100">'.$row["task_log_costcode"].'</td>';
 	$s .= '<td>'.'<a name="tasklog'.@$row['task_log_id'].'"></a>';
 
@@ -108,23 +108,23 @@ foreach ($logs as $row) {
 	$transbrk = "\n[translation]\n";
 	$descrip = str_replace( "\n", "<br />", ($row['task_log_description']) );
 	$tranpos = strpos( $descrip, str_replace( "\n", "<br />", $transbrk ) );
-	if ( $tranpos === false) $s .= $descrip;
-	else
-	{
+	if ( $tranpos === false) { 
+		$s .= $descrip;
+	} else {
 		$descrip = substr( $descrip, 0, $tranpos );
 		$tranpos = strpos( $row['task_log_description'], $transbrk );
 		$transla = substr( $row['task_log_description'], $tranpos + strlen( $transbrk ) );
 		$transla = trim( str_replace( "'", '"', $transla ) );
 		$s .= $descrip."<div style='font-weight: bold; text-align: right'><a title='$transla' class='hilite'>[".$AppUI->_("translation")."]</a></div>";
 	}
-// end auto-translation code
-			
+	// end auto-translation code
+	
 	$s .= '</td>';
 	$s .= "\n\t<td>";
 	if ($canDelete) {
-		$s .= "\n\t\t<a href=\"javascript:delIt2({$row['task_log_id']});\" title=\"".$AppUI->_('delete log')."\">"
-			. "\n\t\t\t". dPshowImage( './images/icons/stock_delete-16.png', 16, 16, '' )
-			. "\n\t\t</a>";
+		$s .= ("\n\t\t<a href=\"javascript:delIt2({$row['task_log_id']});\" title=\"".$AppUI->_('delete log')."\">"
+			   . "\n\t\t\t". dPshowImage( './images/icons/stock_delete-16.png', 16, 16, '' )
+			   . "\n\t\t</a>");
 	}
 	$s .= "\n\t</td>";
 	$s .= '</tr>';
