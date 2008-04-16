5043b4fdd86300b85313017faed1b770b184b34f
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2008-04-16 01:56:40 +0000

    New file to resolve Mantis Report #2300
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5676 c625438d-3aaf-4c84-a37d-fcf3134530f0

---
+++ dotproject/modules/projects/companies_tab.view.projects_gantt.php
@@ -0,0 +1,28 @@
+<?php 
+if (!defined('DP_BASE_DIR')){
+	die('You should not access this file directly.');
+}
+
+global $m, $a, $addPwOiD, $AppUI, $buffer, $company_id, $min_view, $priority, $projects, $tab, $user_id;
+
+$perms =& $AppUI->acl();
+$df = $AppUI->getPref('SHDATEFORMAT');
+
+$pstatus =  dPgetSysVal('ProjectStatus');
+
+$projFilter_extra = array('-4' => 'All w/o archived');
+
+// load the companies class to retrieved denied companies
+require_once($AppUI->getModuleClass('companies'));
+
+// retrieve any state parameters
+if (isset($_GET['tab'])) {
+	$AppUI->setState('DeptProjIdxTab', $_GET['tab']);
+}
+
+$extraGet = '&user_id='.$user_id;
+?>
+<?php
+$min_view = true;
+require(DP_BASE_DIR.'/modules/projects/viewgantt.php');
+?>
