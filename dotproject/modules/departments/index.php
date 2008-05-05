d42bd0336081f272f606545615440c4b67dca956
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2008-05-05 19:46:29 +0000

    "Fix" for Mantis Report 2356.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5700 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/departments/index.php
+++ dotproject/modules/departments/index.php
@@ -4,34 +4,38 @@ if (!defined('DP_BASE_DIR')){
 }
 
 // load the companies class
-require_once( $AppUI->getModuleClass( 'companies' ) );
+require_once($AppUI->getModuleClass('companies'));
 
-if (isset( $_POST['company_id'] ))
-	$AppUI->setState( 'DeptIdxCompany', intval( $_POST['company_id'] ) );
+if (isset($_POST['company_id']))
+	$AppUI->setState('DeptIdxCompany', intval($_POST['company_id']));
 
-$company = $company_id = $AppUI->getState( 'DeptIdxCompany' ) !== NULL ? $AppUI->getState( 'DeptIdxCompany' ) : $AppUI->user_company;
+$company = $company_id = (($AppUI->getState('DeptIdxCompany') !== NULL) 
+                          ? $AppUI->getState('DeptIdxCompany') : $AppUI->user_company);
 
 $company_prefix = 'company_';
 
-if (isset( $_POST['department'] )) {
-	$AppUI->setState( 'DeptIdxDepartment', $_POST['department'] );
+if (isset($_POST['department'])) {
+	$AppUI->setState('DeptIdxDepartment', $_POST['department']);
 	
 	//if department is set, ignore the company_id field
 	unset($company_id);
 }
-$department = $AppUI->getState( 'DeptIdxDepartment' ) !== NULL ? $AppUI->getState( 'DeptIdxDepartment' ) : ($AppUI->user_department > 0 ? $AppUI->user_department : $company_prefix.$AppUI->user_company);
+$department = (($AppUI->getState('DeptIdxDepartment') !== NULL) 
+               ? $AppUI->getState('DeptIdxDepartment') 
+               : (($AppUI->user_department > 0) ? $AppUI->user_department 
+                  : ($company_prefix . $AppUI->user_company)));
 
 $canRead = getPermission($m, 'view', $department);
 if (!$canRead) {
-	$AppUI->redirect( 'm=public&a=access_denied' );
+	$AppUI->redirect('m=public&a=access_denied');
 }
 $AppUI->savePlace();
 
-//if $department contains the $company_prefix string that it's requesting a company and not a department.  So, clear the 
-// $department variable, and populate the $company_id variable.
-if(!(strpos($department, $company_prefix)===false)){
+//if $department contains the $company_prefix string that it's requesting a company 
+//and not a department. So, clear the $department variable, and populate the $company_id variable.
+if (!(strpos($department, $company_prefix)===false)){
 	$company_id = substr($department,strlen($company_prefix));
-	$AppUI->setState( 'DeptIdxCompany', $company_id );
+	$AppUI->setState('DeptIdxCompany', $company_id);
 	unset($department);
 }
 
@@ -60,44 +64,50 @@ $buffer .= '<option value="company_0" style="font-weight:bold;">'.$AppUI->_('All
 $company = '';
 foreach ($rows as $row) {
 	if ($row['dept_parent'] == 0) {
-		if($company!=$row['company_id']){
-			$buffer .= '<option disabled="disabled" value="'.$company_prefix.$row['company_id'].'" style="font-weight:bold;">'.$row['company_name'].'</option>'."\n";
+		if ($company!=$row['company_id']) {
+			$buffer .= ('<option disabled="disabled" value="' . $company_prefix 
+			            . $row['company_id'] . '" style="font-weight:bold;">' 
+			            . $row['company_name'] . '</option>' . "\n");
 			$company=$row['company_id'];
 		}
-		if($row['dept_parent']!=null){
-			showchilddept( $row );
-			findchilddept( $rows, $row['dept_id'] );
+		if ($row['dept_parent']!=null){
+			showchilddept($row);
+			findchilddept($rows, $row['dept_id']);
 		}
 	}
-	if($row['dept_id'] == $department) {
+	if ($row['dept_id'] == $department) {
 		$company_id=$row['dept_company'];
 	}
 }
 $buffer .= '</select>';
 
 // setup the title block
-$titleBlock = new CTitleBlock( 'Departments', 'users.gif', $m, $m.$a );
-$titleBlock->addCrumb( '?m=companies', 'companies list' );
-$titleBlock->addCell( $AppUI->_('Department') . ':');
-$titleBlock->addCell( $buffer, '', '<form action="?m=departments" method="post" name="pickCompany">', '</form>');
+$titleBlock = new CTitleBlock('Departments', 'users.gif', $m, $m.$a);
+$titleBlock->addCrumb('?m=companies', 'companies list');
+$titleBlock->addCell($AppUI->_('Department') . ':');
+$titleBlock->addCell($buffer, '', 
+                     '<form action="?m=departments" method="post" name="pickCompany">', '</form>');
 $titleBlock->addCell();
+/*
+//removing "new department" button from department module, for Mantis Report #2356
 if ($canEdit) {
 	$titleBlock->addCell();
-	$titleBlock->addCell(
-		'<input type="submit" class="button" value="'.$AppUI->_('new department').'">', '',
-		'<form action="?m=departments&a=addedit&company_id='.$company_id.'&dept_parent='.$department.'" method="post">', '</form>'
-	);
+	$titleBlock->addCell('<input type="submit" class="button" value="' 
+	                     . $AppUI->_('new department').'">', '',
+	                     '<form action="?m=departments&a=addedit&company_id=' . $company_id 
+	                     . '&dept_parent=' . $department . '" method="post">', '</form>');
 }
-$titleBlock->addCrumb( '?m=companies', 'company list' );
+*/
+$titleBlock->addCrumb('?m=companies', 'company list');
 
 if ($company_id) {
-	$titleBlock->addCrumb( '?m=companies&a=view&company_id='.$company_id, 'view this company' );
+	$titleBlock->addCrumb('?m=companies&a=view&company_id='.$company_id, 'view this company');
 }
 if ($canEdit && $department > 0) {
-	$titleBlock->addCrumb( '?m=departments&a=addedit&dept_id='.$department, 'edit this department' );
+	$titleBlock->addCrumb('?m=departments&a=addedit&dept_id='.$department, 'edit this department');
 
 	if ($canDelete) {
-		$titleBlock->addCrumbDelete( 'delete department', $canDelete, $msg );
+		$titleBlock->addCrumbDelete('delete department', $canDelete, $msg);
 	}
 }
 $titleBlock->show();
