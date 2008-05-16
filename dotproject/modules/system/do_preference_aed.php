63c33c41838f8bed93ca56dfd6b2201fe37588b7
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2008-05-16 23:31:00 +0000

    Removed call to invalid function dPRealPath().
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5711 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/system/do_preference_aed.php
+++ dotproject/modules/system/do_preference_aed.php
@@ -13,26 +13,26 @@ foreach ($_POST['pref_name'] as $name => $value) {
 	$obj->pref_value = $value;
 
 	// prepare (and translate) the module name ready for the suffix
-	$AppUI->setMsg( 'Preferences' );
+	$AppUI->setMsg('Preferences');
 	if ($del) {
 		if (($msg = $obj->delete())) {
-			$AppUI->setMsg( $msg, UI_MSG_ERROR );
+			$AppUI->setMsg($msg, UI_MSG_ERROR);
 		} else {
-			$AppUI->setMsg( 'deleted', UI_MSG_ALERT, true );
+			$AppUI->setMsg('deleted', UI_MSG_ALERT, true);
 		}
 	} else {
 		if (($msg = $obj->store())) {
-			$AppUI->setMsg( $msg, UI_MSG_ERROR );
+			$AppUI->setMsg($msg, UI_MSG_ERROR);
 		} else {
 			if ($obj->pref_user) {
 			// if user preferences, reload them now
-				$AppUI->loadPrefs( $AppUI->user_id );
+				$AppUI->loadPrefs($AppUI->user_id);
 				$AppUI->setUserLocale();
-				include_once dPRealPath( './locales/'.$AppUI->user_locale.'/locales.php' );
-				include dPRealPath( './locales/core.php' );
-				$AppUI->setMsg( 'Preferences' );
+				include_once (DP_BASE_DIR . '/locales/'.$AppUI->user_locale.'/locales.php');
+				include (DP_BASE_DIR . '/locales/core.php');
+				$AppUI->setMsg('Preferences');
 			}
-			$AppUI->setMsg( 'updated', UI_MSG_OK, true );
+			$AppUI->setMsg('updated', UI_MSG_OK, true);
 		}
 	}
 }
