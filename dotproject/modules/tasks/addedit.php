7aebc0b32573728d1f38efbcb6a1ab031787066c
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2009-05-14 13:14:59 +0000

    Fix for Mantis Report #2622 Base link for TabBox was not properly constructed.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5888 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/tasks/addedit.php
+++ dotproject/modules/tasks/addedit.php
@@ -333,7 +333,9 @@ if (isset($_GET['tab'])) {
 	$AppUI->setState('TaskAeTabIdx', dPgetParam($_GET, 'tab', 0));
 }
 $tab = $AppUI->getState('TaskAeTabIdx', 0);
-$tabBox =& new CTabBox(('m=tasks&a=addedit&task_id=' . $task_id), '', $tab, '');
+$tabBox =& new CTabBox(('?m=tasks&a=addedit' 
+                        . (($task_project) ? '&task_project=' . $task_project 
+                           : '&task_id=' . $task_id)), '', $tab, '');
 $tabBox->add(DP_BASE_DIR.'/modules/tasks/ae_desc', 'Details');
 $tabBox->add(DP_BASE_DIR.'/modules/tasks/ae_dates', 'Dates');
 $tabBox->add(DP_BASE_DIR.'/modules/tasks/ae_depend', 'Dependencies');
