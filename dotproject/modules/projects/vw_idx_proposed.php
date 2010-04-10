e46506a07320ae2d5ae7c60809ab8b1b6497d710
merlinyoda <merlinyoda@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2010-04-10 19:31:45 +0000

    Don't show overlib project description box if there is no description set
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@5975 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/projects/vw_idx_proposed.php
+++ dotproject/modules/projects/vw_idx_proposed.php
@@ -155,11 +155,12 @@ echo htmlspecialchars($row['company_description'], ENT_QUOTES); ?>">
 	</td>
 	<td width="100%">
 		<a href="?m=projects&amp;a=view&amp;project_id=<?php 
-echo htmlspecialchars($row['project_id']); ?>" onmouseover="return overlib('<?php 
+echo htmlspecialchars($row['project_id']); ?>" <?php 
+if (!empty($row['project_description'])) { ?>onmouseover="return overlib('<?php 
 echo(htmlspecialchars(('<div><p>' . str_replace(array("\r\n", "\n", "\r"), '</p><p>', 
                                                 addslashes($row['project_description'])) 
                        . '</p></div>'), ENT_QUOTES)); ?>', CAPTION, '<?php 
-echo($AppUI->_('Description')); ?>', CENTER);" onmouseout="nd();">
+echo($AppUI->_('Description')); ?>', CENTER);" onmouseout="nd();"<?php } ?>>
 		<?php echo (htmlspecialchars($row['project_name'], ENT_QUOTES)); ?>
 		</a>
 	</td>
