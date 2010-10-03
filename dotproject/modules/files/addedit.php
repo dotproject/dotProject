663b537074e4f8a772e79046c325a962e6b397c5
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2010-10-03 12:03:20 +0000

    Don't use url encoding in JS functions as per comment on Firefox bug with window.open
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@6044 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/modules/files/addedit.php
+++ dotproject/modules/files/addedit.php
@@ -129,7 +129,7 @@ function popTask() {
     if (f.file_project.selectedIndex == 0) {
         alert("<?php echo $AppUI->_('Please select a project first!', UI_OUTPUT_JS); ?>");
     } else {
-        window.open('?m=public&amp;a=selector&amp;dialog=1&amp;callback=setTask&amp;table=tasks&amp;task_project='
+        window.open('?m=public&a=selector&dialog=1callback=setTask&table=tasks&task_project='
 		            + f.file_project.options[f.file_project.selectedIndex].value, 'task','left=50,top=50,height=250,width=400,resizable')
     }
 }
