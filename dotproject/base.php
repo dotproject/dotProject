2904832a4c32e0d70ea3d23722ef87b352c71367
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2012-06-13 06:05:05 +0000

    #2901 - restrict warnings on various versions of PHP
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@6162 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/base.php
+++ dotproject/base.php
@@ -23,7 +23,7 @@
 ini_set('display_errors', 1);
 
 if (defined('E_DEPRECATED')) {
-	error_reporting(E_ALL & ~(E_DEPRECATED|E_NOTICE));
+	error_reporting(E_ALL & ~(E_DEPRECATED|E_NOTICE|E_STRICT));
 } else {
 	error_reporting(E_ALL & ~E_NOTICE);
 }
