ce0568387a44545a01b4782a12f39912ba729ca2
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2012-08-14 08:21:23 +0000

    Add table fixups for those that were broken in the 2.1.5 install
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@6178 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/db/upgrade_215_to_216.sql
+++ dotproject/db/upgrade_215_to_216.sql
@@ -24,3 +24,33 @@ UPDATE `%dbprefix%sysvals` SET `sysval_value` = '0|admin@example.com\n1|admin@ex
 # 20120605
 # Extend the key size on the sessions table
 ALTER TABLE `%dbprefix%sessions` CHANGE `session_id` `session_id` VARCHAR(60) NOT NULL;
+
+# 20120814
+# Fix up table errors in the 2.1.5 install
+CREATE TABLE IF NOT EXISTS `%dbprefix%common_notes` (
+  `note_id` int(10) unsigned NOT NULL auto_increment,
+  `note_author` int(10) unsigned NOT NULL default '0',
+  `note_module` int(10) unsigned NOT NULL default '0',
+  `note_record_id` int(10) unsigned NOT NULL default '0',
+  `note_category` int(3) unsigned NOT NULL default '0',
+  `note_title` varchar(100) NOT NULL default '',
+  `note_body` text NOT NULL,
+  `note_date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `note_hours` float NOT NULL default '0',
+  `note_code` varchar(8) NOT NULL default '',
+  `note_created` datetime NOT NULL default '0000-00-00 00:00:00',
+  `note_modified` timestamp,
+  `note_modified_by` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`note_id`)
+) ; 
+
+CREATE TABLE IF NOT EXISTS `%dbprefix%user_access_log` (
+`user_access_log_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`user_id` INT( 10 ) UNSIGNED NOT NULL ,
+`user_ip` VARCHAR( 15 ) NOT NULL ,
+`date_time_in` DATETIME DEFAULT '0000-00-00 00:00:00',
+`date_time_out` DATETIME DEFAULT '0000-00-00 00:00:00',
+`date_time_last_action` DATETIME DEFAULT '0000-00-00 00:00:00',
+PRIMARY KEY ( `user_access_log_id` )
+);
+
