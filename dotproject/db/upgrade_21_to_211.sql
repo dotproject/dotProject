a925c4a33f727ef2a7f7ee4fc690579eec4762b4
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2012-11-14 01:32:58 +0000

    Remove bogus date which breaks 2.1.1 to 2.1.2 upgrade.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@6184 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/db/upgrade_21_to_211.sql
+++ dotproject/db/upgrade_21_to_211.sql
@@ -6,6 +6,6 @@
 # All entries must be date stamped in the correct format.
 #
 #20071113
-#20080622 - change to allow for a db table prefix
+# change to allow for a db table prefix
 # Remove the NOT NULL clause from company_description to avoid issues on win plaforms
 ALTER TABLE `%dbprefix%companies` MODIFY `company_description` text;
