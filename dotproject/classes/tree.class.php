e43f8bafbc1938f3931df5b85aaea99e63318d4c
ajdonnison <ajdonnison@c625438d-3aaf-4c84-a37d-fcf3134530f0>
2014-05-26 06:53:11 +0000

    Improve sorting on tasklist report.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dotproject/code/branches/stable_2@6240 c625438d-3aaf-4c84-a37d-fcf3134530f0

--- dotproject/classes/tree.class.php
+++ dotproject/classes/tree.class.php
@@ -10,14 +10,17 @@ if (! defined('DP_BASE_DIR')) {
  */
 class CDpTree
 {
-	var $base_node;
+	private $base_node;
 
 	/**
-	 * Constructor.  This is PHP4 compliant.
+	 * Constructor. 
 	 */
-	function CDpTree() {
+	public function __construct($sort_key = null) {
 		$emptynode = null;
 		$this->base_node = new CDpTreeNode(0, $emptynode);
+		if ($sort_key) {
+			$this->base_node->set_sort_key($sort_key);
+		}
 	}
 
 	/**
@@ -29,7 +32,7 @@ class CDpTree
 	 * @param mixed $data
 	 * @return void
 	 */
-	function add($parent, $id, &$data) {
+	public function add($parent, $id, &$data) {
 		if ($parent == $id) {
 			$this->base_node->add($id, $data);
 		} else {
@@ -46,7 +49,7 @@ class CDpTree
 	/**
 	 * Return the node whose Id matches
 	 */
-	function &find_node($parent) {
+	public function &find_node($parent) {
 		return $this->base_node->find($parent);
 	}
 
@@ -55,20 +58,22 @@ class CDpTree
 	 * Descend the tree applying the callback to each element.
 	 * The callback is passed the data block and a depth indication.
 	 */
-	function display($method) {
-		$this->base_node->display($method);
+	public function display($method) {
+		$this->base_node->display($method, $this->sort_key);
 	}
 }
 
 class CDpTreeNode
 {
-	var $data = null;
-	var $id = null;
-	var $children = array();
-	var $depth = 0;
-	var $parent = null;
+	private $data = null;
+	private $id = null;
+	private $children = array();
+	private $depth = 0;
+	private $parent = null;
+	private $sort_key = null;
+	private $sorted = false;
 
-	function CDpTreeNode($id = 0, &$data = null, &$parent = null) {
+	public function __construct($id = 0, &$data = null, &$parent = null) {
 		$this->id = $id;
 		if (isset($data)) {
 			$this->data =& $data;
@@ -76,16 +81,20 @@ class CDpTreeNode
 		if (isset($parent)) {
 			$this->parent =& $parent;
 			$this->depth = $this->parent->depth  + 1;
+			$this->sort_key = $parent->sort_key;
 		}
 		$this->children = array();
 	}
 
-	function add($id, &$data) {
-		$this->children[$id] = new CDpTreeNode($id, $data, $this);
-
+	public function set_sort_key($key = null) {
+		$this->sort_key = $key;
 	}
 
-	function &find($id) {
+	public function add($id, &$data) {
+		$this->children[] = new CDpTreeNode($id, $data, $this);
+	}
+
+	public function &find($id) {
 		$result = false;
 		if ($this->id == $id) {
 			return $this;
@@ -100,12 +109,29 @@ class CDpTreeNode
 		return $result;
 	}
 
-	function display($method) {
+	public function display($method) {
 		reset($this->children);
+		if ($this->sort_key && ! $this->sorted) {
+			// Sort based on the supplied sort key
+			usort($this->children, array($this, 'do_sort'));
+		}
 		while (list($id, $node) = each($this->children)) {
 			call_user_func($method, $node->depth, $node->data);
 			$node->display($method);
 		}
+	}
+
+	protected function do_sort($a, $b) {
+		$key = (array)$this->sort_key;
+		foreach ($key as $part) {
+			if ($a->data[$part] < $b->data[$part]) {
+				return -1;
+			}
+			elseif ($a->data[$part] > $b->data[$part]) {
+				return 1;
+			}
+		}
+		return 0;
 	}
 
 }
