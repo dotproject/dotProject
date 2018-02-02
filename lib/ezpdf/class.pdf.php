51f029d7913c5db2deee9ddf48584f5c1b49d73d
Adam Donnison <ajdonnison@gmail.com>
2018-02-02 19:11:18 +1100

    Remove deprecated set_magic_quotes_runtime calls

--- lib/ezpdf/class.pdf.php
+++ lib/ezpdf/class.pdf.php
@@ -1511,9 +1511,9 @@ function selectFont($fontName,$encoding='',$set=1){
         // simple utility to convert them from pfa to pfb.
         $fp = fopen($fbfile,'rb');
         $tmp = get_magic_quotes_runtime();
-        set_magic_quotes_runtime(0);
+        #set_magic_quotes_runtime(0);
         $data = fread($fp,filesize($fbfile));
-        set_magic_quotes_runtime($tmp);
+        #set_magic_quotes_runtime($tmp);
         fclose($fp);
 
         // create the font descriptor
@@ -2638,7 +2638,7 @@ function addPngFromFile($file,$x,$y,$w=0,$h=0){
   // read in a png file, interpret it, then add to the system
   $error=0;
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+  #set_magic_quotes_runtime(0);
   $fp = @fopen($file,'rb');
   if ($fp){
     $data='';
@@ -2650,7 +2650,7 @@ function addPngFromFile($file,$x,$y,$w=0,$h=0){
     $error = 1;
     $errormsg = 'trouble opening file: '.$file;
   }
-  set_magic_quotes_runtime($tmp);
+  #set_magic_quotes_runtime($tmp);
   
   if (!$error){
     $header = chr(137).chr(80).chr(78).chr(71).chr(13).chr(10).chr(26).chr(10);
@@ -2853,9 +2853,9 @@ function addJpegFromFile($img,$x,$y,$w=0,$h=0){
   $fp=fopen($img,'rb');
 
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+  #set_magic_quotes_runtime(0);
   $data = fread($fp,filesize($img));
-  set_magic_quotes_runtime($tmp);
+  #set_magic_quotes_runtime($tmp);
   
   fclose($fp);
 
@@ -2904,7 +2904,7 @@ function addImage(&$img,$x,$y,$w=0,$h=0,$quality=75){
   $fp=fopen($tmpName,'rb');
 
   $tmp = get_magic_quotes_runtime();
-  set_magic_quotes_runtime(0);
+  #set_magic_quotes_runtime(0);
   $fp = @fopen($tmpName,'rb');
   if ($fp){
     $data='';
@@ -2917,7 +2917,7 @@ function addImage(&$img,$x,$y,$w=0,$h=0,$quality=75){
     $errormsg = 'trouble opening file';
   }
 //  $data = fread($fp,filesize($tmpName));
-  set_magic_quotes_runtime($tmp);
+  #set_magic_quotes_runtime($tmp);
 //  fclose($fp);
   unlink($tmpName);
   $this->addJpegImage_common($data,$x,$y,$w,$h,$imageWidth,$imageHeight);
