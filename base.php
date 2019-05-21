4232aaa834cdbf89fcf75317da310e93e2482b74
Abner Rizzi <bacteria666@gmail.com>
2019-05-21 08:28:40 -0300

    Update base.php

--- base.php
+++ base.php
@@ -63,7 +63,7 @@ $baseUrl = (((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off')
              ? 'https://' : 'http://');
 $baseUrl .= safe_get_env('HTTP_HOST');
 // check if webserver is not running on default port                
-if ($_SERVER['SERVER_PORT'] != 80 || $_SERVER['SERVER_PORT'] '443') {
+if ($_SERVER['SERVER_PORT'] != 80 || $_SERVER['SERVER_PORT'] != 443) {
   $baseUrl .= ':' . safe_get_env('SERVER_PORT');
 }
 $pathInfo = safe_get_env('PATH_INFO');
