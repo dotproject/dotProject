a746914b4d71300292ea606b93ec85840e0c3b34
Matt Bell <matt@2pisoftware.com>
2019-06-06 22:41:21 +1000

    Begin rich edit dp class

---
+++ classes/richedit.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class DpRichEdit {
+    function __construct() {
+        
+    }
+
+    static function Header() {
+        echo '<script src="../quill.min.js"></script>
+        <link rel="stylesheet" href="../quill.snow.css" />';
+    }
+}
