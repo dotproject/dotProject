10f91f81e4e3aa9b4b12263153c37a5b50d1b2cf
Adam Donnison <ajdonnison@gmail.com>
2020-03-12 12:00:17 +1100

    Cleanup for 2.2.0 version

--- db/upgrade_latest.php
+++ db/upgrade_latest.php
@@ -29,7 +29,7 @@ require_once DP_BASE_DIR.'/classes/permissions.class.php';
  */
 function dPupgrade($from_version, $to_version, $last_updated) {
 
-	$latest_update = '20180525'; // Set to the latest upgrade date.
+	$latest_update = '20200312'; // Set to the latest upgrade date.
 
 	if (empty($last_updated) || empty($from_version)) {
 		$last_updated = '00000000';
@@ -165,6 +165,7 @@ function dPupgrade($from_version, $to_version, $last_updated) {
 		case '20120814':
 		case '20130727':
                 case '20180525':
+		case '20200312':
 		// TODO:  Add new versions here.  Keep this message above the default label.
 		default:
 			break;
