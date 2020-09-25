0c855d73314957499c2414478abfbbc2747f2303
jphilapy <jeff@altahost.com>
2020-09-25 13:26:09 -0400

    Test that user can signin

--- tests/acceptance/SigninCest.php
+++ tests/acceptance/SigninCest.php
@@ -11,10 +11,22 @@ class SigninCest
     // tests
     public function weAreAbleToLoginIn(AcceptanceTester $I)
     {
+        // TODO: Add to this test the ability to auto-create a test user
         $I->amOnPage('/index.php');
         $I->fillField('username', 'admin');
         $I->fillField('password', 'pass');
         $I->click(['class' => 'button']);
-        $I->see('My Project');
+        $I->see('My Project');// if text not found, test fails
+    }
+
+
+    public function weDontSeeErrorsOnPage(AcceptanceTester $I)
+    {
+        // TODO: Add to this test the ability to auto-create a test user
+        $I->amOnPage('/index.php');
+        $I->fillField('username', 'admin');
+        $I->fillField('password', 'pass');
+        $I->click(['class' => 'button']);
+        $I->dontSee('ERROR: '); //if text is found, test fails.
     }
 }
