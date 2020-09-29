98ad609279d007cc527dcf528e2ce9e6c2009221
jphilapy <jeff@altahost.com>
2020-09-29 12:07:52 -0400

    made class easier to test

--- classes/authenticator.class.php
+++ classes/authenticator.class.php
@@ -158,18 +158,28 @@ if (!defined('DP_BASE_DIR')) {
 		}
 	}
 
+	/**
+	 * Authenticate user against database
+	 */
 	class SQLAuthenticator
 	{
 		var $user_id;
 		var $username;
 
+		/**
+		 * Query database and get user to compare
+		 *
+		 * @param [type] $username
+		 * @param [type] $password
+		 * @return void
+		 */
 		function authenticate($username, $password)
 		{
 			GLOBAL $db, $AppUI;
 
 			$this->username = $username;
 
-			$q  = new DBQuery;
+			$q  = $this->getDBQuery();
 			$q->addTable('users');
 			$q->addQuery('user_id, user_password');
 			$q->addWhere("user_username = '$username'");
@@ -184,13 +194,32 @@ if (!defined('DP_BASE_DIR')) {
 
 			$this->user_id = $row["user_id"];
 			$q->clear();
-			if (MD5($password) == $row["user_password"]) return true;
+
+			return $this->comparePasswords($password, $row["user_password"]);
+		}
+
+		/**
+		 * Verifies that passwords are the same
+		 *
+		 * @param [string] $fPassword ex: from form
+		 * @param [string] $dbPassword ex: from database
+		 * @return boolean
+		 */
+		function comparePasswords($fPassword, $dbPassword) {
+			if (MD5($fPassword) == $dbPassword) return true;
 			return false;
+		}
+
+		/**
+		 * separated out so that authenticate() can be unit tested
+		 */
+		function getDBQuery() {
+			return new DBQuery;
 		}
 
 		function userId($username)
 		{
-                        // We ignore the username provided
+            // We ignore the username provided
 			return $this->user_id;
 		}
 	}	
