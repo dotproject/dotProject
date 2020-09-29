3bb4cbcd85cef62e943e7450cdce4b5bcd772107
jphilapy <jeff@altahost.com>
2020-09-29 13:18:21 -0400

    re worked test

--- tests/unit/DotProjectBaseClassTest.php
+++ tests/unit/DotProjectBaseClassTest.php
@@ -1,11 +1,12 @@
 <?php 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 define('DP_BASE_DIR', '.'); // need to set this, or files can't be referenced.
+define('UNIT_TEST', true); // need to set this, or files can't be referenced.
 
 require_once('./tests/autoload.php');
 require_once('./includes/main_functions.php');
 
-$AppUI = new CAppUI;
+// $AppUI = new CAppUI;
 
 class DotProjectBaseClassTest extends \Codeception\Test\Unit
 {
@@ -23,16 +24,16 @@ class DotProjectBaseClassTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testGetErrorMethodReturnsMessage()
     {
-        $this->makeEmpty('CAppUI');
-        $CDpObject = $this->makeEmptyExcept('CDpObject', '_error', 'getError'); //return $this->_error;
-        $CDpObject = $this->makeEmptyExcept('DBQuery', 'dPgetConfig'); //return $this->_error;
+        
+        $CDpObject = $this->makeEmptyExcept('CDpObject',  'getError', ['_error' => 'Error Message']); //return $this->_error;
+        // $DBQuery = $this->makeEmptyExcept('DBQuery', 'dPgetConfig'); //return $this->_error;
 
         $actual = $CDpObject->getError();
 
         
-        $this->assertTrue($actual);
+        $this->assertSame('Error Message',$actual);
 
     }
 }
