ff52c0aa844d47979930591284cfa656257d0def
jphilapy <jeff@altahost.com>
2020-09-29 12:09:07 -0400

    used clear test name

---
+++ tests/unit/AuthenticatorTest.php
@@ -0,0 +1,46 @@
+<?php 
+define('DP_BASE_DIR', true); // need to set this, or files can't be referenced.
+
+
+require_once('./tests/autoload.php');
+
+
+class AuthenticatorTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+        
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testThePasswordsAreTheSame()
+    {
+
+        $test = $this->makeEmptyExcept('SQLAuthenticator', 'comparePasswords');
+
+        $actual = $test->comparePasswords('whatacoolpassword', MD5('whatacoolpassword'));
+
+         $this->assertTrue($actual);
+
+    }
+
+    public function testIfPasswordsAreNotTheSameFalseIsReturned()
+    {
+
+        $test = $this->makeEmptyExcept('SQLAuthenticator', 'comparePasswords');
+
+        $actual = $test->comparePasswords('whatacoolpassword', MD5('notthesamepassword'));
+
+         $this->assertFalse($actual);
+
+    }
+}
