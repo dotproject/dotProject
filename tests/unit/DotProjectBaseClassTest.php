f7095676bf6227c202386b2c8ae60e72381527c3
jphilapy <jeff@altahost.com>
2020-10-01 10:58:00 -0400

    adding new test

--- tests/unit/DotProjectBaseClassTest.php
+++ tests/unit/DotProjectBaseClassTest.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 define('DP_BASE_DIR', '.'); // need to set this, or files can't be referenced.
 define('UNIT_TEST', true); // need to set this, or files can't be referenced.
 
-require_once('./includes/main_functions.php');
-require_once('./classes/dp.class.php');
+require_once './includes/main_functions.php';
+require_once './classes/dp.class.php';
+require_once './classes/query.class.php';
+require_once './modules/system/system.class.php';
 
 class DotProjectBaseClassTest extends \Codeception\Test\Unit
 {
@@ -12,7 +14,7 @@ class DotProjectBaseClassTest extends \Codeception\Test\Unit
      * @var \UnitTester
      */
     protected $tester;
-    
+
     protected function _before()
     {
     }
@@ -24,14 +26,13 @@ class DotProjectBaseClassTest extends \Codeception\Test\Unit
     // tests
     public function testGetErrorMethodReturnsMessage()
     {
-        
-        $CDpObject = $this->makeEmptyExcept('CDpObject',  'getError', ['_error' => 'Error Message']); //return $this->_error;
+
+        $CDpObject = $this->makeEmptyExcept('CDpObject', 'getError', ['_error' => 'Error Message']); //return $this->_error;
         // $DBQuery = $this->makeEmptyExcept('DBQuery', 'dPgetConfig'); //return $this->_error;
 
         $actual = $CDpObject->getError();
 
-        
-        $this->assertSame('Error Message',$actual);
+        $this->assertSame('Error Message', $actual);
 
     }
 
@@ -39,25 +40,24 @@ class DotProjectBaseClassTest extends \Codeception\Test\Unit
     public function testCanGetModuleDirectoryIfPassedIn()
     {
 
-        $CDpObject = $this->makeEmptyExcept('CDpObject',  'getModuleName', ['_module_directory' => './modules']); //return $this->_error;
-        // $DBQuery = $this->makeEmptyExcept('DBQuery', 'dPgetConfig'); //return $this->_error;
+        $CDpObject = $this->makeEmptyExcept('CDpObject', 'getModuleName', ['_module_directory' => './modules']); 
 
         $actual = $CDpObject->getModuleName();
 
-        
-        $this->assertSame('./modules',$actual);
+        $this->assertSame('./modules', $actual);
     }
-    
-        /** @test */
-        public function testCanGetModuleDirectoryIfPassedIn2()
-        {
-    
-            $CDpObject = $this->makeEmptyExcept('CDpObject',  'getModuleName', ['_module_directory' => './modules']); //return $this->_error;
-            // $DBQuery = $this->makeEmptyExcept('DBQuery', 'dPgetConfig'); //return $this->_error;
-    
-            $actual = $CDpObject->getModuleName();
-    
-            
-            $this->assertSame('./modules',$actual);
-        }
+
+    /** @test */
+    public function testCanGetModuleByName()
+    {
+        // TODO: Get this test working
+        // $CModule = $this->makeEmpty('CModule');
+        // $CModule = $this->makeEmpty('DBQuery');
+
+        // $CDpObject = $this->makeEmptyExcept('CDpObject', 'getModuleName', ['_module_directory' => '', '_permission_name', 'admin']);
+        
+        // $actual = $CDpObject->getModuleName();
+
+        // $this->assertSame('./modules', $actual);
+    }
 }
