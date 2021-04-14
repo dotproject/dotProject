45dd174ac91fe71faf24ba27af8f4d44d2facbf7
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-14 16:24:23 +0000

    ‘new’ by reference is not allowed by PHP 8

--- lib/PEAR/FixPHP5PEARWarnings.php
+++ lib/PEAR/FixPHP5PEARWarnings.php
@@ -1,7 +1,15 @@
 <?php
 if ($skipmsg) {
-    $a = &new $ec($code, $mode, $options, $userinfo);
+  if (version_compare(phpversion(), '8.0.0', '<')) {
+    $a = &new $ec($code, $mode, $options, $userinfo);  // PHP 8 doesn't need/like references (gwyneth 20210414)
+  } else {
+    $a = new $ec($code, $mode, $options, $userinfo);
+  }
 } else {
-    $a = &new $ec($message, $code, $mode, $options, $userinfo);
+  if (version_compare(phpversion(), '8.0.0', '<')) {
+    $a = &new $ec($message, $code, $mode, $options, $userinfo);  // PHP 8 doesn't need/like references (gwyneth 20210414)
+  } else {
+    $a = new $ec($message, $code, $mode, $options, $userinfo);
+  }
 }
 ?>
