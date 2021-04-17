1a9e97790a4363e49cb726594923ff358ff2ac42
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-17 09:13:49 +0000

    Making sure that we’re using a valid date format…

--- modules/projects/viewgantt.php
+++ modules/projects/viewgantt.php
@@ -36,13 +36,13 @@ if ($showAllGantt!='0') {
 if (isset($_POST['proFilter'])) {
 	$AppUI->setState('ProjectIdxFilter',  $_POST['proFilter']);
 }
-$proFilter = (($AppUI->getState('ProjectIdxFilter') !== NULL) 
+$proFilter = (($AppUI->getState('ProjectIdxFilter') !== NULL)
               ? $AppUI->getState('ProjectIdxFilter') : '-1');
 
 
 $projectStatus = dPgetSysVal('ProjectStatus');
-$projFilter = arrayMerge(array('-1' => 'All Projects', '-2' => 'All w/o in progress', 
-                               '-3' => (($AppUI->user_id == $user_id) ? 'My projects' 
+$projFilter = arrayMerge(array('-1' => 'All Projects', '-2' => 'All w/o in progress',
+                               '-3' => (($AppUI->user_id == $user_id) ? 'My projects'
                                         : "User's projects")), $projectStatus);
 if (!(empty($projFilter_extra))) {
 	$projFilter = arrayMerge($projFilter, $projFilter_extra);
@@ -57,6 +57,9 @@ $display_option = dPgetCleanParam($_POST, 'display_option', 'this_month');
 
 // format dates
 $df = $AppUI->getPref('SHDATEFORMAT');
+if (empty($df)) {
+  $df = "YYYY-MM-DD";  // better safe than sorry... (gwyneth 20210417)
+}
 
 if ($display_option == 'custom') {
 	// custom dates
@@ -84,7 +87,7 @@ if (!@$min_view) {
 function scrollPrev() {
 	f = document.editFrm;
 <?php
-$new_start = new CDate($start_date);	
+$new_start = new CDate($start_date);
 $new_start->day = 1;
 $new_end = new CDate($end_date);
 $new_start->addMonths(-$scroll_date);
@@ -102,7 +105,7 @@ function scrollNext() {
 <?php
 $new_start = new CDate($start_date);
 $new_start->day = 1;
-$new_end = new CDate($end_date);	
+$new_end = new CDate($end_date);
 $new_start->addMonths($scroll_date);
 $new_end->addMonths($scroll_date);
 echo "f.sdate.value='" . $new_start->format(FMT_TIMESTAMP_DATE) . "';";
@@ -126,7 +129,7 @@ function showFullProject() {
 <table class="tbl" width="100%" border="0" cellpadding="4" cellspacing="0" summary="projects view gantt">
 <tr>
 	<td>
-		<form name="editFrm" method="post" action="?<?php 
+		<form name="editFrm" method="post" action="?<?php
 foreach ($_GET as $key => $val) {
 	$url_query_string .= (($url_query_string) ? '&amp;' : '') . $key . '=' . $val;
 }
@@ -138,7 +141,7 @@ echo ($url_query_string);
 			<td align="left" valign="top" width="20">
 <?php if ($display_option != "all") { ?>
 				<a href="javascript:scrollPrev()">
-				<img src="./images/prev.gif" width="16" height="16" alt="<?php 
+				<img src="./images/prev.gif" width="16" height="16" alt="<?php
 	echo $AppUI->_('previous');?>" border="0" />
 				</a>
 <?php } ?>
@@ -154,37 +157,37 @@ echo $start_date->format(FMT_DATE_HTML5);?>" class="text dpDateField">
 echo $end_date->format(FMT_DATE_HTML5);?>" class="text dpDateField">
 			</td>
 			<td valign="top">
-				<?php 
+				<?php
 echo arraySelect($projFilter, 'proFilter', 'size="1" class="text"', $proFilter, true);?>
 			</td>
 			<td valign="top">
-				<input type="checkbox" name="showLabels" id="showLabels" value='1' <?php 
-echo (($showLabels==1) ? 'checked="checked"' : "");?> /><label for="showLabels"><?php 
+				<input type="checkbox" name="showLabels" id="showLabels" value='1' <?php
+echo (($showLabels==1) ? 'checked="checked"' : "");?> /><label for="showLabels"><?php
 echo $AppUI->_('Show captions');?></label>
 			</td>
 			<td valign="top">
-				<input type="checkbox" value='1' name="showInactive" id="showInactive" <?php 
-echo (($showInactive==1) ? 'checked="checked"' : "");?> /><label for="showInactive"><?php 
+				<input type="checkbox" value='1' name="showInactive" id="showInactive" <?php
+echo (($showInactive==1) ? 'checked="checked"' : "");?> /><label for="showInactive"><?php
 echo $AppUI->_('Show Archived');?></label>
 			</td>
 			<td valign="top">
-				<input type="checkbox" value='1' name="showAllGantt" id="showAllGantt" <?php 
-echo (($showAllGantt==1) ? 'checked="checked"' : "");?> /><label for="showAllGantt"><?php 
+				<input type="checkbox" value='1' name="showAllGantt" id="showAllGantt" <?php
+echo (($showAllGantt==1) ? 'checked="checked"' : "");?> /><label for="showAllGantt"><?php
 echo $AppUI->_('Show Tasks');?></label>
 			</td>
 			<td valign="top">
-				<input type="checkbox" value='1' name="sortTasksByName" id="sortTasksByName" <?php 
-echo (($sortTasksByName==1) ? 'checked="checked"' : "");?> /><label for="sortTasksByName"><?php 
+				<input type="checkbox" value='1' name="sortTasksByName" id="sortTasksByName" <?php
+echo (($sortTasksByName==1) ? 'checked="checked"' : "");?> /><label for="sortTasksByName"><?php
 echo $AppUI->_('Sort Tasks By Name');?></label>
 			</td>
 			<td align="left">
-				<input type="button" class="button" value="<?php 
+				<input type="button" class="button" value="<?php
 echo $AppUI->_('submit');?>" onclick='document.editFrm.display_option.value="custom";submit();' />
 			</td>
 			<td align="right" valign="top" width="20">
 <?php if ($display_option != "all") { ?>
 			<a href="javascript:scrollNext()">
-				<img src="./images/next.gif" width="16" height="16" alt="<?php 
+				<img src="./images/next.gif" width="16" height="16" alt="<?php
 echo $AppUI->_('next');?>" border="0" />
 			</a>
 <?php } ?>
@@ -192,9 +195,9 @@ echo $AppUI->_('next');?>" border="0" />
 		</tr>
 		<tr>
 			<td align="center" valign="bottom" colspan="12">
-				<?php 
-echo ("<a href='javascript:showThisMonth()'>" . $AppUI->_('show this month') 
-	. "</a> : <a href='javascript:showFullProject()'>" . $AppUI->_('show all') . "</a><br />"); 
+				<?php
+echo ("<a href='javascript:showThisMonth()'>" . $AppUI->_('show this month')
+	. "</a> : <a href='javascript:showFullProject()'>" . $AppUI->_('show all') . "</a><br />");
 ?>
 			</td>
 		</tr>
@@ -205,20 +208,20 @@ echo ("<a href='javascript:showThisMonth()'>" . $AppUI->_('show this month')
 		<tr>
 			<td>
 				<?php
-$src = ("?m=projects&amp;a=gantt&amp;suppressHeaders=1" . 
-	(($display_option == 'all') ? '' 
-         : ('&amp;start_date=' . $start_date->format("%Y-%m-%d") 
-           . '&amp;end_date=' . $end_date->format("%Y-%m-%d"))) . "&amp;width='" 
-		. "+((navigator.appName=='Netscape'?window.innerWidth:document.body.offsetWidth)*0.95)" 
-		. "+'&amp;showLabels=" . $showLabels . '&amp;sortTasksByName=' .$sortTasksByName 
-		. '&amp;proFilter=' .$proFilter . '&amp;showInactive=' . $showInactive 
-		. '&amp;company_id=' . $company_id . '&amp;department=' . $department . '&amp;dept_ids=' . $dept_ids 
-		. '&amp;showAllGantt=' . $showAllGantt . '&amp;user_id=' . $user_id . '&amp;addPwOiD=' . $addPwOiD 
+$src = ("?m=projects&amp;a=gantt&amp;suppressHeaders=1" .
+	(($display_option == 'all') ? ''
+         : ('&amp;start_date=' . $start_date->format("%Y-%m-%d")
+           . '&amp;end_date=' . $end_date->format("%Y-%m-%d"))) . "&amp;width='"
+		. "+((navigator.appName=='Netscape'?window.innerWidth:document.body.offsetWidth)*0.95)"
+		. "+'&amp;showLabels=" . $showLabels . '&amp;sortTasksByName=' .$sortTasksByName
+		. '&amp;proFilter=' .$proFilter . '&amp;showInactive=' . $showInactive
+		. '&amp;company_id=' . $company_id . '&amp;department=' . $department . '&amp;dept_ids=' . $dept_ids
+		. '&amp;showAllGantt=' . $showAllGantt . '&amp;user_id=' . $user_id . '&amp;addPwOiD=' . $addPwOiD
 		. '&amp;m_orig=' . $m_orig . '&amp;a_orig=' . $a_orig);
 echo '<script>document.write(\'<img src="' . $src . '">\')</script>';
 if (!dPcheckMem(32*1024*1024)) {
 	echo '</td></tr><tr><td>';
-	echo ('<span style="color: red; font-weight: bold;">'  . $AppUI->_('invalid memory config') 
+	echo ('<span style="color: red; font-weight: bold;">'  . $AppUI->_('invalid memory config')
 	      . '</span>');
 }
 ?>
