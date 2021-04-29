6de1bd831359cf402fa54a38ee4ae0ab23f0ff88
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-29 18:20:38 +0000

    … better do a search & replace to correct all spelling mistakes…

--- style/dp-grey-theme/lostpass.php
+++ style/dp-grey-theme/lostpass.php
@@ -47,7 +47,7 @@
 	echo '<span class="error">'.$AppUI->getMsg().'</span>';
 
 	$msg = '';
-	$msg .=  phpversion() < '4.1' ? '<br /><span class="warning">WARNING: dotproject is NOT SUPPORT for this PHP Version ('.phpversion().')</span>' : '';
+	$msg .=  phpversion() < '4.1' ? '<br /><span class="warning">WARNING: dotproject is NOT SUPPORTED for this PHP Version ('.phpversion().')</span>' : '';
 	echo $msg;
 ?>
 </div>
