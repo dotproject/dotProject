eb9b445b2aaf665d05354a7ca755ef1bc55ed5f4
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-05-01 09:58:09 +0000

    Added debugging code for `$dPconfig['overlib_extra_parameters’]`

--- modules/projects/admin_tab.viewuser.projects.php
+++ modules/projects/admin_tab.viewuser.projects.php
@@ -209,7 +209,8 @@ echo htmlspecialchars(('<div><p>' . str_replace(array("\r\n", "\n", "\r"), '</p>
                                                 addslashes($row['project_description']))
                        . '</p></div>'), ENT_QUOTES);
 ?>', CAPTION, '<?php echo $AppUI->_('Description'); ?>', CENTER<?php $dPconfig['overlib_extra_parameters'] ?? '' ?>);" onmouseout="nd();">
-		<?php echo htmlspecialchars($row['project_name'], ENT_QUOTES); ?>
+		<?php echo htmlspecialchars($row['project_name'], ENT_QUOTES);
+    dprint(__FILE__, __LINE__, 11, "[DEBUG]: Extra parameters for overLib: '" . ($dPconfig['overlib_extra_parameters'] ?? '[empty]') . "'"); ?>
 		</a>
 	</td>
 	<td align="center"><?php
